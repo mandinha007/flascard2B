d704a1bc11d439f05ab920eb6fd1e2632d452294
Mendonça <132940434+mandinha007@users.noreply.github.com>
2024-11-05 09:56:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'Spider-men',
+    'Quem foi a primeira namorada do Peter Park? ',
+    'Betty Brant'
+)
+criaCartao(
+    'Spider-men',
+    'Qual o nome completo do Homem Aranha?',
+    ''
+)
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
