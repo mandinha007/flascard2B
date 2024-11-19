416d3e59e8679bde327a645f19dad9148723140f
Mendonça <132940434+mandinha007@users.noreply.github.com>
2024-11-19 10:04:25 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -6,10 +6,18 @@ criaCartao(
 criaCartao(
     'Spider-men',
     'Qual o nome completo do Homem Aranha?',
-    ''
+    ' Peter Benjamin Parker'
 )
 criaCartao(
-    categoria,
-    pergunta,
-    resposta
+    'Spider-men',
+    'Quem é o melhor amigo do Peter Parker?',
+    'Harry Osborn'
 )
+
+criaCartao(
+    'Spider-men',
+    'Quantos anos tinha Peter Parker quando foi picado?',
+    '15 anos'
+)
+
+   
